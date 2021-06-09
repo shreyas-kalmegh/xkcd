c0256f2661aa7c7f33c34966badbe2f16a81382f
shreyas kalmegh <skalmegh@redhat.com>
2021-06-10 03:16:14 +0530

    add a print statement as a prompt for task_one

--- task_one.py
+++ task_one.py
@@ -2,4 +2,5 @@ import sys
 from src.main.task_one_helpers import *
 
 if __name__ == "__main__":
+    print("Would you like to read a comic? Type 'python task_one.py -h' and enter to get help on how to do so.")
     main(sys.argv[1:])
